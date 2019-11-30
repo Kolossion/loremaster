f443d5c86baf056eac0276d23bd236bbd9d66736
Kolossion <gamester4433@gmail.com>
2019-11-30 00:59:54 -0800

    Initial tests and fun. Working better than blessed.

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+  import TermBox from './components/TermBox.svelte'
+</script>
+
+<main>
+  <article>
+    <TermBox title="Hello!" color="#777">
+      laksdjflakjsdflkjasldfkja
+      sdflaksjdflkjasdlfkjasd
+      falskdjfalskjdflakjsdfa
+      sdflkajsdflkjasldkfjas
+      dfalskdjfalskjdflakjsdflkjasdf
+      asdlfkjasdlkfjaslkdfj
+    </TermBox>
+    <TermBox title="Hello!" color="#07f">
+      laksdjflakjsdflkjasldfkja
+      sdflaksjdflkjasdlfkjasd
+      falskdjfalskjdflakjsdfa
+      sdflkajsdflkjasldkfjas
+      dfalskdjfalskjdflakjsdflkjasdf
+      asdlfkjasdlkfjaslkdfj
+    </TermBox>
+  </article>
+</main>
+
+<style>
+main {
+  width: 100%;
+  height: 100%;
+}
+
+article {
+  display: flex;
+  height: 100vh;
+}
+</style>
---
+++ src/components/TermBox.svelte
@@ -0,0 +1,47 @@
+<script>
+  export let title;
+  export let color;
+</script>
+
+<style>
+.term-box {
+  font-family: 'Source Code Pro', monospace;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  padding: 24px;
+}
+
+.outline {
+  border-width: 2px;
+  border-style: solid;
+  outline-offset: -7px;
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  width: calc(100% - 20px);
+  height: calc(100% - 20px);
+  z-index: 1;
+}
+
+.title {
+  display: inline-block;
+  font-weight: bold;
+  padding-left: 5px;
+  padding-right: 5px;
+  background-color: #333;
+  color: white;
+  z-index: 2;
+  position:absolute;
+  top: 0;
+  left: 24px;
+}
+</style>
+
+<div class="term-box">
+  <div class="outline" style={"border-color: " + color + ";"}></div>
+  <span class="title">{title}</span>
+  <div class="content">
+    <slot></slot>
+  </div>
+</div>
